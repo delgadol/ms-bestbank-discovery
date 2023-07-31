609c9b7682c4909dafb5ce100cd1802e74015056
Luis Delgado <37703993+delgadol@users.noreply.github.com>
2023-07-30 19:27:23 -0500

    Actualizaciones

--- src/test/java/com/bestbank/servdiscovery/MsBestbankDiscoveryApplicationTests.java
+++ src/test/java/com/bestbank/servdiscovery/MsBestbankDiscoveryApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bestbank.servdiscovery;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {com.bestbank.servdiscovery.MsBestbankDiscoveryApplication.class})
 class MsBestbankDiscoveryApplicationTests {
 
 	@Test
+	@Disabled("Tes de prueba")
 	void contextLoads() {
+	  Assertions.fail();
 	}
 
 }
